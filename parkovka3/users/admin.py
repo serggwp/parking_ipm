ccf162b9867ce6a4b208dcbb68f15aa25c6f778c
serggwp <sergeeva.2sasha@gmail.com>
2024-03-05 19:30:34 +0300

    1.0

--- parkovka3/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from users.models import User
-
-class CustomUserAdmin(BaseUserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'phone_number' )
-
-admin.site.register(User, CustomUserAdmin)
